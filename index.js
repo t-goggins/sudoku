42d0244a275dddf52434d28642a05646d84c9337
t-goggins <thomas.a.goggins@gmail.com>
2021-05-09 22:22:57 +0100

    Added grid styling to the layout

--- index.js
+++ index.js
@@ -2,36 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
+/**
+ * An individual square which contains a number 
+ */
 function Square(){
-  /* An individual square which contains a number */
   return (
-    <button>
-      "X"
+    <button className="square">
+      X
     </button>
   );
 }
 
+
+/**
+ * A 3x3 Box (made up of 9 individual squares)
+ */
 class Box extends React.Component {
-  /* A 3x3 Box (made up of 9 individual squares)*/ 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: this.props.boxes
+    }
+  }
+
   renderSquare() {
     return <Square />
   }
 
   render() {
     return (
-      <div style={{ border: "2px solid black", display: "grid"}}>
-        <div className="box-row" style={{ border: "2px solid blue", display: "grid", gridTemplateColumns: "1fr 1fr 1fr"}}>
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
-        </div>
+      <div className="box">
+        {this.renderSquare()}
+        {this.renderSquare()}
+        {this.renderSquare()}
+        {this.renderSquare()}
+        {this.renderSquare()}
+        {this.renderSquare()}
+        {this.renderSquare()}
+        {this.renderSquare()}
+        {this.renderSquare()}
       </div>
     );
   }
@@ -39,13 +49,9 @@ class Box extends React.Component {
 
 class Grid extends React.Component {
   /* A 9x9 Grid (made up of 9x Boxes) */
-  renderBox() {
-    return <Box />
-  }
-
   render() {
     return (
-      <div className="grid-row" style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr"}}>
+      <div className="grid">
         <Box />
         <Box />
         <Box />
@@ -61,10 +67,17 @@ class Grid extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boxes: Array(9).fill(null)
+    }
+  }
+
   render() {
     return (
-      <div style={{ width: "400px", height: "400px"}}>
-        <Grid />
+      <div>
+        <Grid boxes={this.state.boxes}/>
       </div>
     );
   }
